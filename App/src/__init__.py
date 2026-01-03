b5d6041a1ae49f505ad6a8fe1fed01cd5298e567
er-henish-ai <henishbhanderi666@gmail.com>
2026-01-03 23:48:08 +0530

    commit

--- App/src/__init__.py
+++ App/src/__init__.py
@@ -1,18 +1,11 @@
 from flask import Flask
-
-
-
-
 def create_app():
     app = Flask(
         __name__,
         static_folder="../static",
         template_folder="../templates"
     )
-@app.route("/")
-def home():
-    return "FunZone is alive"
-
-    app.config.from_object("App.src.config")
-
+    @app.route("/")
+    def home():
+        return "FunZone is alive"
     return app
