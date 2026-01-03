fb392eba1d2db14425ea48c919d180f64d742913
er-henish-ai <henishbhanderi666@gmail.com>
2026-01-04 00:06:34 +0530

    database connection

--- App/src/__init__.py
+++ App/src/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(
         __name__,
         static_folder="../static",
         template_folder="../templates"
     )
+
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+
     @app.route("/")
     def home():
         return "FunZone is alive"
+
     return app
