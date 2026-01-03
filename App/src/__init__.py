2b079ebfeb71582b6d79da968496e65191660ca2
er-henish-ai <henishbhanderi666@gmail.com>
2026-01-03 23:36:50 +0530

    commit

--- App/src/__init__.py
+++ App/src/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(
-        __name__,
-        static_folder="../static",
-        template_folder="../templates"
-    )
-@app.route("/")
-def home():
-    return "FunZone is alive"
 
-    app.config.from_object("App.src.config")
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "Environment Set Successfully!"
 
     return app
